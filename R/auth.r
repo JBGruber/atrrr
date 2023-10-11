70f0889d3d12117bd6e9d3e5a868b0a7aef61ada
ben <bguinaudeau@gmail.com>
2023-10-11 09:09:21 -0400

    reexpose print

--- R/auth.r
+++ R/auth.r
@@ -159,6 +159,7 @@ refresh_token <- function(token) {
 #' @description Print a AT token
 #' @param x An object of class \code{bsky_token}
 #' @param ... not used.
+#' @export
 print.bsky_token <- function(x, ...) {
   cli::cli_h1("Blue Sky token")
   cli::cat_bullet(glue::glue("User: {x$handle}"),
