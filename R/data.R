682b89b2eb6227dc4c104935fc57609787390be7
JBGruber <JohannesB.Gruber@gmail.com>
2024-01-04 17:16:55 +0100

    update readme

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #' A list with all lexicons available for the AT protocols.
 #' \describe{
 #'   \item{names}{Name of the lexicon}
-#'   \item{values}{path relative to <https://github.com/bluesky-social/atproto/blob/main/lexicons>}
+#'   \item{values}{path relative to <https://github.com/bluesky-social/atproto/tree/main/lexicons>}
 #' }
 #' @source <https://github.com/bluesky-social/atproto>
 "list_lexicons"
