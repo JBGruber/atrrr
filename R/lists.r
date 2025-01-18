953849796e94592b2af732741dbb62a226ba9907
JBGruber <JohannesB.Gruber@gmail.com>
2025-01-18 22:49:14 +0100

    adds get_list_feed

--- R/lists.r
+++ R/lists.r
@@ -20,3 +20,69 @@ get_starter_pack <- function(starter_pack,
   return(parse_list(res))
 
 }
+
+
+#' Title
+#'
+#' Get a feed of recent posts from a list (posts and reposts from any actors on
+#' the list).
+#'
+#' @param list The url of the requested list
+#' @inheritParams get_feed
+#'
+#' @returns a data frame (or nested list) of posts
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # use the URL of a list
+#' get_feed("https://bsky.app/profile/smachlis.bsky.social/lists/3l7o5d7b7nl2q")
+#' }
+get_list_feed <- function(list,
+                          limit = 25,
+                          cursor = NULL,
+                          parse = TRUE,
+                          verbose = NULL,
+                          .token = NULL) {
+
+  res <- list()
+  req_limit <- ifelse(limit > 100, 100, limit)
+  last_cursor <- NULL
+
+  if (verbosity(verbose)) cli::cli_progress_bar(
+    format = "{cli::pb_spin} Got {length(res)} skeets, but there is more.. [{cli::pb_elapsed}]",
+    format_done = "Got {length(res)} records. All done! [{cli::pb_elapsed}]"
+  )
+
+  feed_url <- convert_http_to_at(list, .token = .token)
+
+  while (length(res) < limit) {
+    resp <- do.call(
+      what = app_bsky_feed_get_list_feed,
+      args = list(
+        list = feed_url,
+        limit = req_limit,
+        cursor = last_cursor,
+        .token = .token,
+        .return = "json"
+      ))
+
+    last_cursor <- resp$cursor
+    res <- c(res, resp$feed)
+
+    if (is.null(resp$cursor)) break
+    if (verbosity(verbose)) cli::cli_progress_update(force = TRUE)
+  }
+
+  if (verbosity(verbose)) cli::cli_progress_done()
+
+  if (parse) {
+    if (verbosity(verbose)) cli::cli_progress_step("Parsing {length(res)} results.")
+    out <- parse_feed(res)
+    if (verbosity(verbose)) cli::cli_process_done(msg_done = "Got {nrow(out)} results. All done!")
+  } else {
+    out <- res
+  }
+  attr(out, "last_cursor") <- last_cursor
+  return(out)
+}
